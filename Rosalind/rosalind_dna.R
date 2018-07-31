6311a9fcb046ec5887141c6054e2ed30ed0af0e4
Matt Malishev <matthew.malishev@gmail.com>
2018-07-31 17:40:39 -0400

    rm

--- Rosalind/rosalind_dna.R
+++
@@ -1,4 +0,0 @@
-setwd("/Users/malishev/Documents/Data/Rosalind/Rosalind") 
-install.packages("stringr"); library(stringr)
-s <- read.csv("rosalind_dna.txt",header=F,sep=",",stringsAsFactors = F);s
-str_count(s,c("A","C","G","T"))
